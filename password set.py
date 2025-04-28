91f4893e485f074fbcb6a9ccf6fc0dff5734f1ca
Ucheawaji Anne Dienye <dienyeucheawaji93@gmail.com>
2025-04-28 12:02:22 +0100

    password set

---
+++ password set.py
@@ -0,0 +1,8 @@
+#coding
+password1 = input("Enter a new password: ")
+password2 = input("Re-enter the password: ")
+
+if password1 == password2:
+    print("Password Set")
+else:
+    print("Error: Passwords do not match.")
