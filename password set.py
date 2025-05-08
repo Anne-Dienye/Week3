de347e373a73a3158b7a0dbd5ef3af1e49ba813e
Anne-Dienye <dienyeucheawaji93@gmail.com>
2025-05-09 00:17:59 +0100

    Delete password set.py

--- password set.py
+++
@@ -1,8 +0,0 @@
-#coding
-password1 = input("Enter a new password: ")
-password2 = input("Re-enter the password: ")
-
-if password1 == password2:
-    print("Password Set")
-else:
-    print("Error: Passwords do not match.")
