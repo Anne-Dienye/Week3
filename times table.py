f6a8420eea34cc163592a79a00c707e3eeccc636
Anne-Dienye <dienyeucheawaji93@gmail.com>
2025-05-09 00:17:47 +0100

    Delete times table.py

--- times table.py
+++
@@ -1,7 +0,0 @@
-#coding
-table_number = int(input("Enter the number for the times table (0-12): "))
-if 0 <= table_number <= 12:
-    for i in range(13):
-        print(f"{i} x {table_number} = {i * table_number}")
-else:
-    print("Error: Number must be between 0 and 12.")
